3880ddb65a4ee4804f320fa3fd207c6dc9066421
Jonathan Wade <jonathan.wade@quantum-health.com>
2023-10-05 10:28:43 -0400

    Added Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,20 @@ module.exports = {
     title: `Splash`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7YFH7VH54R", // Google Analytics / GA
+        ],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
+    "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
